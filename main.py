17f1614fe6efb42b8b857370fc6fa8a162cfd8e7
jackguo2016 <923081130@qq.com>
2022-11-21 23:22:09 -0500

    update

--- main.py
+++ main.py
@@ -7,7 +7,17 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-"hi"
+    pxy1 = normpdf(xTe, 0, 1)
+    pxxy1 = pxy1[:, 0] * pxy1[:, 1]
+    pxy2 = normpdf(xTe, OFFSET, 1)
+    pxxy2 = pxy2[:, 0] * pxy2[:, 1]
+
+    pxx = pxxy1 * 0.5 + pxxy2 * 0.5
+
+    py1xx = pxxy1 * 0.5 / pxx
+    py2xx = pxxy2 * 0.5 / pxx
+
+    ybar = py1xx + 2 * py2xx
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
